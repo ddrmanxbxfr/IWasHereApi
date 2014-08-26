9d50e1841af0b05c1cfafeb051561575922835c3
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-08-26 00:21:47 -0400

    Revert "Ajout API Base"
    This reverts commit ac5d5e6258402f2a892f46fb31d642ba14294a42.

--- api/doc/vendor/api_project.js
+++
@@ -1,9 +0,0 @@
-define({
-  "name": "APES API",
-  "version": "0.0.1",
-  "description": "Documentation de l'api APES. Fonctions variés.",
-  "generator": {
-    "version": "0.6.0",
-    "time": "2014-08-10T03:28:17.114Z"
-  }
-});
