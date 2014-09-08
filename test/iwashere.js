6d3c3f27e7400b615629ede4c7846f840375d8fd
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-08 03:13:56 -0400

    added more validation test

--- test/iwashere.js
+++ test/iwashere.js
@@ -2,7 +2,7 @@ var iwashere = require('./../lib/iwashere.js');
 var should = require('should');
 
 describe('Verifier les contributions des utilisateurs', function () {
-    var itShouldHave1UserContribution, itShouldHave0Contribution;
+    var itShouldHave1UserContribution, itShouldHave0Contribution, itShouldHaveOneFeatures;
     before(function (done) {
         var mockDataEmpty = {
             features: [
@@ -18,11 +18,22 @@ describe('Verifier les contributions des utilisateurs', function () {
 
         itShouldHave0Contribution = iwashere.ajouterProps({});
 
+        mockDataEmpty = {
+            features: [
+                {
+                    properties: {
+                        textNote: "yolo"
+                    }
+                }
+            ]
+        };
+
+        itShouldHaveOneFeatures = iwashere.ajouterProps(mockDataEmpty);
 
         done();
     });
 
-    it('should have 0 contributions', function () {
+    it('should have 0 contributions even with empty json object', function () {
         itShouldHave0Contribution.name.should.be.exactly("ParkingAPI").and.be.a.String;
         itShouldHave0Contribution.type.should.be.exactly("FeaturesCollection").and.be.a.String;
         itShouldHave0Contribution.features.length.should.be.exactly(0).and.be.a.Number;
@@ -34,5 +45,13 @@ describe('Verifier les contributions des utilisateurs', function () {
         itShouldHave1UserContribution.type.should.be.exactly("FeaturesCollection").and.be.a.String;
         itShouldHave1UserContribution.features.length.should.be.exactly(0).and.be.a.Number;
         itShouldHave1UserContribution.properties.nbSansContributions.should.be.exactly(1).and.be.a.Number;
+    });
+
+        it('should have 1 features with textProp', function () {
+        itShouldHaveOneFeatures.name.should.be.exactly("ParkingAPI").and.be.a.String;
+        itShouldHaveOneFeatures.type.should.be.exactly("FeaturesCollection").and.be.a.String;
+        itShouldHaveOneFeatures.features.length.should.be.exactly(1).and.be.a.Number;
+        itShouldHaveOneFeatures.features[0].properties.textNote.should.be.exactly("yolo").and.be.a.String;
+        itShouldHaveOneFeatures.properties.nbSansContributions.should.be.exactly(0).and.be.a.Number;
     });
 });
